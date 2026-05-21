97d59fba6b37e945c6e24e35fe1a4b016bcd0f51
AsherMeow <aphroditeleebrown@gmail.com>
2026-05-21 10:58:05 +0100

    inital poo

---
+++ script.js
@@ -0,0 +1,24 @@
+function homeButton() {
+    document.getElementById("homeBoxID").style.display = "block";
+    document.getElementById("addBoxID").style.display = "none";
+    document.getElementById("ocsBoxID").style.display = "none";
+    document.getElementById("loreBoxID").style.display = "none";
+}
+function plusButton() {
+    document.getElementById("homeBoxID").style.display = "none";
+    document.getElementById("addBoxID").style.display = "block";
+    document.getElementById("ocsBoxID").style.display = "none";
+    document.getElementById("loreBoxID").style.display = "none";
+}
+function ocsButton() {
+    document.getElementById("homeBoxID").style.display = "none";
+    document.getElementById("addBoxID").style.display = "none";
+    document.getElementById("ocsBoxID").style.display = "block";
+    document.getElementById("loreBoxID").style.display = "none";
+}
+function loreButton() {
+    document.getElementById("homeBoxID").style.display = "none";
+    document.getElementById("addBoxID").style.display = "none";
+    document.getElementById("ocsBoxID").style.display = "none";
+    document.getElementById("loreBoxID").style.display = "block";
+}
